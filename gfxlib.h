9c6ac17819420182fac83a709fccdf81b44e61f1
Van Nguyen <vannn@568e.vn>
2023-11-17 21:18:07 +0700

    update version

--- gfxlib.h
+++ gfxlib.h
@@ -52,8 +52,8 @@
 #pragma warning(disable: 26485 26481 26408 26826 26814 26438 26448 26475 6001)
 
 //GFX version string
-#define GFX_VERSION             "v23.05.22"
-#define GFX_BUILD_ID            20230522
+#define GFX_VERSION             "v23.10.14"
+#define GFX_BUILD_ID            20231014
 
 //MIXED mode constants
 #define SCREEN_WIDTH            640     //default screen size
